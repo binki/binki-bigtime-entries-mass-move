e6006a147660e0117e3a1e9fbd89c2cfd1992e8c
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2024-04-06 22:50:27 -0400

    binki-bigtime-entries-mass-move-1.0.0

---
+++ README.md
@@ -0,0 +1,5 @@
+Automatically change the date of multiple entries in Day view.
+
+When stopping timers, the values are all added to the current day. Oftentimes, I stop timers after midnight—either a few minutes after or the next morning. If I performed many tasks the prior day, it is tedious to move all of the entries over. This provides a new “Move All” button which, while clunky, speeds up that process.
+
+[Install](binki-bigtime-entries-mass-move.user.js?raw=1)
---
+++ binki-bigtime-entries-mass-move.user.js
@@ -0,0 +1,90 @@
+// ==UserScript==
+// @name     binki-bigtime-entries-mass-move
+// @homepageURL https://github.com/binki/binki-bigtime-entries-mass-move
+// @version  1.0.0
+// @match https://intuit.bigtime.net/bigtime
+// @match https://intuit.bigtime.net/bigtime/*
+// @match https://intuit.bigtime.net/Bigtime
+// @match https://intuit.bigtime.net/Bigtime/*
+// @require https://github.com/binki/binki-userscript-when-element-query-selector-async/raw/0a9c204bdc304a9e82f1c31d090fdfdf7b554930/binki-userscript-when-element-query-selector-async.js
+// @require https://github.com/binki/binki-userscript-delay-async/raw/252c301cdbd21eb41fa0227c49cd53dc5a6d1e58/binki-userscript-delay-async.js
+// @require https://github.com/binki/binki-userscript-when-element-changed-async/raw/88cf57674ab8fcaa0e86bdf5209342ec7780739a/binki-userscript-when-element-changed-async.js
+// @require https://github.com/binki/binki-userscript-when-event-dispatched-async/raw/0daa1c0c3501aeba7132d520aa8f389e0627aba6/binki-userscript-when-event-dispatched-async.js
+// ==/UserScript==
+
+(async () => {
+  console.log('loaded');
+  function testIfHrefIsDaily() {
+    return /#\/timesheet\/daily\/\d+\/\d+$/.test(window.location.href);
+  }
+  while (true) {
+    if (!testIfHrefIsDaily()) {
+      await whenEventDispatchedAsync(window, 'hashchange');
+      continue;
+    }
+    console.log('on daily timesheet screen');
+  	const dailyPlannerDataUl = await whenElementQuerySelectorAsync(document.body, 'ul.DailyPlannerData');
+    if (!testIfHrefIsDaily()) {
+      continue;
+    }
+    const uiLi = document.createElement('li');
+    const moveAllButton = document.createElement('button');
+    moveAllButton.textContent = 'Move All';
+    moveAllButton.type = 'button';
+    moveAllButton.addEventListener('click', async () => {
+      moveAllButton.disabled = true;
+      try {
+				const form = await whenElementQuerySelectorAsync(document.body, 'form[validation-object=selectedEntry]');
+        const countEntries = () => dailyPlannerDataUl.querySelectorAll('li.entry').length;
+        let entryCount = countEntries();
+        const chosenDate = await (async () => {
+          const entry = dailyPlannerDataUl.querySelector('li.entry');
+          entry.click();
+          const messageDiv = document.createElement('div');
+          messageDiv.textContent = 'Choose the date and press Save to move all.';
+          document.body.prepend(messageDiv);
+          try {
+            while (form.parentElement.classList.contains('ng-hide')) await whenElementChangedAsync(form);
+            const saveButton = await whenElementQuerySelectorAsync(form, '.btn.btn-primary[ng-bind=saveDefaultTitle]');
+            await whenEventDispatchedAsync(saveButton, 'click');
+            const dateField = form.querySelector('input[input-date][ng-model="selectedEntry.Dt"]');
+            return dateField.value;
+          } finally {
+            document.body.removeChild(messageDiv);
+          }
+        })();
+        while (true) {
+          // Wait for the previous thing to save.
+          console.log(`waiting for entries to reduce from ${entryCount}`);
+          while (countEntries() === entryCount) await whenElementChangedAsync(dailyPlannerDataUl);
+          entryCount = countEntries();
+          console.log(`now there are ${entryCount} entries`);
+          // Apparently extra waiting is still necessary for some reason?
+          const entry = dailyPlannerDataUl.querySelector('li.entry');
+          if (!entry) {
+            console.log('no more entries');
+            break;
+          }
+          entry.click();
+          console.log('clicked next');
+          while (form.parentElement.classList.contains('ng-hide')) await whenElementChangedAsync(form.parentElement);
+          const dateField = await whenElementQuerySelectorAsync(form, 'input[input-date][ng-model="selectedEntry.Dt"]');
+          await delayAsync(10);
+          dateField.value = chosenDate;
+          dateField.dispatchEvent(new InputEvent('input', {
+            bubbles: true,
+            cancelable: true,
+            data: chosenDate,
+          }));
+          (await whenElementQuerySelectorAsync(form, '.btn.btn-primary[ng-bind=saveDefaultTitle]')).click();
+        }
+      } finally {
+        moveAllButton.disabled = false;
+      }
+    });
+    uiLi.appendChild(moveAllButton);
+    dailyPlannerDataUl.appendChild(uiLi);
+    await whenEventDispatchedAsync(window, 'hashchange');
+    dailyPlannerDataUl.removeChild(uiLi);
+  }
+})();
